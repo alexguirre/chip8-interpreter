9014a1d37f205a20e85f76427c051443b995d07a
alexguirre <alexguirre.aa@gmail.com>
2019-09-18 22:31:05 +0200

    Add SDL2
    Using vcpkg

--- src/interpreter/main.cpp
+++ src/interpreter/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window* window = SDL_CreateWindow(
+		"The window",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		640, 480,
+		SDL_WINDOW_OPENGL);
+
+	if (!window)
+	{
+		std::cerr << "Failed to create window\n";
+		return 1;
+	}
+
+	SDL_Delay(5000);
+
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
+	return 0;
 }
