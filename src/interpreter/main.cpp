331c833e0e3c5851dfaf88fbe01aa36427cdaf36
alexguirre <alexguirre.aa@gmail.com>
2019-09-22 18:42:23 +0200

    Add commandline support

--- src/interpreter/main.cpp
+++ src/interpreter/main.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 #include <vector>
-
+#include <tclap/CmdLine.h>
 #include "Interpreter.h"
 
-int main(int /*argc*/, char* /*argv*/[])
+int main(int argc, char* argv[])
 {
+	TCLAP::CmdLine cmd("Chip-8 interpreter", ' ', "WIP");
+	TCLAP::UnlabeledValueArg<std::string> inputArg("input_file", "Specifies the filename of the program ROM.", true, "", "input_file");
+
+	cmd.add(inputArg);
+
+	cmd.parse(argc, argv);
+
 	SDL_Init(SDL_INIT_VIDEO);
+
+	try
 	{
 		CInterpreter interpreter;
-		interpreter.LoadProgram(R"(E:\programming\chip8\roms\demos\Zero Demo [zeroZshadow, 2007].ch8)");
+		interpreter.LoadProgram(inputArg.getValue());
 	
 		bool quit = false;
 		while (!quit)
@@ -25,6 +34,11 @@ int main(int /*argc*/, char* /*argv*/[])
 			interpreter.Update();
 		}
 	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
 	SDL_Quit();
 
 	return 0;
