d8823d068f7c15175ff73ab3310e1f0ee02ea3dc
alexguirre <alexguirre.aa@gmail.com>
2019-09-29 19:45:27 +0200

    Rename instruction handlers

--- src/interpreter/Instructions.cpp
+++ src/interpreter/Instructions.cpp
@@ -65,29 +65,29 @@ static std::string ToString_NAME_dst_Vx(const SInstruction& i, const SContext& c
 	return ss.str();
 }
 
-static void CLS_Handler(SContext& c)
+static void Handler_CLS(SContext& c)
 {
 	std::fill(c.PixelBuffer.begin(), c.PixelBuffer.end(), std::uint8_t(0));
 	c.PixelBufferDirty = true;
 }
 
-static void RET_Handler(SContext& c)
+static void Handler_RET(SContext& c)
 {
 	c.PC = c.Stack[--c.SP];
 }
 
-static void JP_Handler(SContext& c)
+static void Handler_JP_nnn(SContext& c)
 {
 	c.PC = c.NNN();
 }
 
-static void CALL_Handler(SContext& c)
+static void Handler_CALL_nnn(SContext& c)
 {
 	c.Stack[c.SP++] = c.PC;
 	c.PC = c.NNN();
 }
 
-static void SE_Handler(SContext& c)
+static void Handler_SE_Vx_kk(SContext& c)
 {
 	if (c.V[c.X()] == c.KK())
 	{
@@ -95,7 +95,7 @@ static void SE_Handler(SContext& c)
 	}
 }
 
-static void SNE_Handler(SContext& c)
+static void Handler_SNE_Vx_kk(SContext& c)
 {
 	if (c.V[c.X()] != c.KK())
 	{
@@ -103,7 +103,7 @@ static void SNE_Handler(SContext& c)
 	}
 }
 
-static void SE_2_Handler(SContext& c)
+static void Handler_SE_Vx_Vy(SContext& c)
 {
 	if (c.V[c.X()] == c.V[c.Y()])
 	{
@@ -111,37 +111,37 @@ static void SE_2_Handler(SContext& c)
 	}
 }
 
-static void LD_Handler(SContext& c)
+static void Handler_LD_Vx_kk(SContext& c)
 {
 	c.V[c.X()] = c.KK();
 }
 
-static void ADD_Handler(SContext& c)
+static void Handler_ADD_Vx_kk(SContext& c)
 {
 	c.V[c.X()] += c.KK();
 }
 
-static void LD_2_Handler(SContext& c)
+static void Handler_LD_Vx_Vy(SContext& c)
 {
 	c.V[c.X()] = c.V[c.Y()];
 }
 
-static void OR_Handler(SContext& c)
+static void Handler_OR_Vx_Vy(SContext& c)
 {
 	c.V[c.X()] |= c.V[c.Y()];
 }
 
-static void AND_Handler(SContext& c)
+static void Handler_AND_Vx_Vy(SContext& c)
 {
 	c.V[c.X()] &= c.V[c.Y()];
 }
 
-static void XOR_Handler(SContext& c)
+static void Handler_XOR_Vx_Vy(SContext& c)
 {
 	c.V[c.X()] ^= c.V[c.Y()];
 }
 
-static void ADD_2_Handler(SContext& c)
+static void Handler_ADD_Vx_Vy(SContext& c)
 {
 	std::uint8_t& vx = c.V[c.X()];
 	std::uint8_t& vy = c.V[c.Y()];
@@ -149,7 +149,7 @@ static void ADD_2_Handler(SContext& c)
 	vx += vy;
 }
 
-static void SUB_Handler(SContext& c)
+static void Handler_SUB_Vx_Vy(SContext& c)
 {
 	std::uint8_t& vx = c.V[c.X()];
 	std::uint8_t& vy = c.V[c.Y()];
@@ -157,14 +157,14 @@ static void SUB_Handler(SContext& c)
 	vx -= vy;
 }
 
-static void SHR_Handler(SContext& c)
+static void Handler_SHR_Vx(SContext& c)
 {
 	std::uint8_t& vx = c.V[c.X()];
 	c.V[0xF] = vx & 1;
 	vx >>= 1;
 }
 
-static void SUBN_Handler(SContext& c)
+static void Handler_SUBN_Vx_Vy(SContext& c)
 {
 	std::uint8_t& vx = c.V[c.X()];
 	std::uint8_t& vy = c.V[c.Y()];
@@ -172,14 +172,14 @@ static void SUBN_Handler(SContext& c)
 	vx = vy - vx;
 }
 
-static void SHL_Handler(SContext& c)
+static void Handler_SHL_Vx(SContext& c)
 {
 	std::uint8_t& vx = c.V[c.X()];
 	c.V[0xF] = (vx >> 7) & 1;
 	vx <<= 1;
 }
 
-static void SNE_2_Handler(SContext& c)
+static void Handler_SNE_Vx_Vy(SContext& c)
 {
 	if (c.V[c.X()] != c.V[c.Y()])
 	{
@@ -187,17 +187,17 @@ static void SNE_2_Handler(SContext& c)
 	}
 }
 
-static void LD_I_Handler(SContext& c)
+static void Handler_LD_I_nnn(SContext& c)
 {
 	c.I = c.NNN();
 }
 
-static void JP_2_Handler(SContext& c)
+static void Handler_JP_V0_nnn(SContext& c)
 {
 	c.PC = c.NNN() + c.V[0];
 }
 
-static void RND_Handler(SContext& c)
+static void Handler_RND_Vx_kk(SContext& c)
 {
 	static std::mt19937 gen{ std::random_device{}() };
 	static std::uniform_int_distribution<std::int32_t> dist{
@@ -208,7 +208,7 @@ static void RND_Handler(SContext& c)
 	c.V[c.X()] = rnd & c.KK();
 }
 
-static void DRW_Handler(SContext& c)
+static void Handler_DRW_Vx_Vy_n(SContext& c)
 {
 	std::uint8_t vx = c.V[c.X()];
 	std::uint8_t vy = c.V[c.Y()];
@@ -238,7 +238,7 @@ static void DRW_Handler(SContext& c)
 	c.PixelBufferDirty = true;
 }
 
-static void SKP_Handler(SContext& c)
+static void Handler_SKP_Vx(SContext& c)
 {
 	std::uint8_t vx = c.V[c.X()];
 	if (c.Keyboard[vx])
@@ -247,7 +247,7 @@ static void SKP_Handler(SContext& c)
 	}
 }
 
-static void SKNP_Handler(SContext& c)
+static void Handler_SKNP_Vx(SContext& c)
 {
 	std::uint8_t vx = c.V[c.X()];
 	if (!c.Keyboard[vx])
@@ -256,12 +256,12 @@ static void SKNP_Handler(SContext& c)
 	}
 }
 
-static void LD_3_Handler(SContext& c)
+static void Handler_LD_Vx_DT(SContext& c)
 {
 	c.V[c.X()] = c.DT;
 }
 
-static void LD_4_Handler(SContext& c)
+static void Handler_LD_Vx_K(SContext& c)
 {
 	for (std::uint8_t key = 0; key < c.Keyboard.size(); key++)
 	{
@@ -276,29 +276,29 @@ static void LD_4_Handler(SContext& c)
 	c.PC -= CInterpreter::InstructionByteSize;
 }
 
-static void LD_5_Handler(SContext& c)
+static void Handler_LD_DT_Vx(SContext& c)
 {
 	c.DT = c.V[c.X()];
 }
 
-static void LD_6_Handler(SContext& c)
+static void Handler_LD_ST_Vx(SContext& c)
 {
 	c.ST = c.V[c.X()];
 }
 
-static void ADD_I_Handler(SContext& c)
+static void Handler_ADD_I_Vx(SContext& c)
 {
 	std::uint8_t& vx = c.V[c.X()];
 	c.V[0xF] = static_cast<std::int32_t>(vx) + static_cast<std::int32_t>(c.I) > std::numeric_limits<std::uint8_t>::max();
 	c.I += vx;
 }
 
-static void LD_7_Handler(SContext& c)
+static void Handler_LD_F_Vx(SContext& c)
 {
 	c.I = CInterpreter::FontsetCharByteSize * c.V[c.X()];
 }
 
-static void LD_8_Handler(SContext& c)
+static void Handler_LD_B_Vx(SContext& c)
 {
 	std::uint8_t vx = c.V[c.X()];
 	std::uint8_t ones = vx % 10;
@@ -310,7 +310,7 @@ static void LD_8_Handler(SContext& c)
 	c.Memory[c.I + 2] = ones;
 }
 
-static void LD_9_Handler(SContext& c)
+static void Handler_LD_derefI_Vx(SContext& c)
 {
 	std::uint8_t x = c.X();
 	for (std::uint8_t i = 0; i <= x; i++)
@@ -320,7 +320,7 @@ static void LD_9_Handler(SContext& c)
 	c.I += x + 1;
 }
 
-static void LD_10_Handler(SContext& c)
+static void Handler_LD_Vx_derefI(SContext& c)
 {
 	std::uint8_t x = c.X();
 	for (std::uint8_t i = 0; i <= x; i++)
@@ -334,38 +334,38 @@ using namespace std::placeholders;
 
 const std::vector<SInstruction> SInstruction::InstructionSet =
 {
-	{ "CLS",	CLS_Handler,	0x00E0,	0xF0FF,	ToString_NAME									},
-	{ "RET",	RET_Handler,	0x00EE,	0xF0FF,	ToString_NAME									},
-	{ "JP",		JP_Handler,		0x1000,	0xF000,	ToString_NAME_nnn								},
-	{ "CALL",	CALL_Handler,	0x2000,	0xF000,	ToString_NAME_nnn								},
-	{ "SE",		SE_Handler,		0x3000,	0xF000,	ToString_NAME_Vx_kk								},
-	{ "SNE",	SNE_Handler,	0x4000,	0xF000,	ToString_NAME_Vx_kk								},
-	{ "SE",		SE_2_Handler,	0x5000,	0xF00F,	ToString_NAME_Vx_Vy								},
-	{ "LD",		LD_Handler,		0x6000,	0xF000,	ToString_NAME_Vx_kk								},
-	{ "ADD",	ADD_Handler,	0x7000,	0xF000,	ToString_NAME_Vx_kk								},
-	{ "LD",		LD_2_Handler,	0x8000,	0xF00F,	ToString_NAME_Vx_Vy								},
-	{ "OR",		OR_Handler,		0x8001,	0xF00F,	ToString_NAME_Vx_Vy								},
-	{ "AND",	AND_Handler,	0x8002,	0xF00F,	ToString_NAME_Vx_Vy								},
-	{ "XOR",	XOR_Handler,	0x8003,	0xF00F,	ToString_NAME_Vx_Vy								},
-	{ "ADD",	ADD_2_Handler,	0x8004,	0xF00F,	ToString_NAME_Vx_Vy								},
-	{ "SUB",	SUB_Handler,	0x8005,	0xF00F,	ToString_NAME_Vx_Vy								},
-	{ "SHR",	SHR_Handler,	0x8006,	0xF00F,	ToString_NAME_Vx								},
-	{ "SUBN",	SUBN_Handler,	0x8007,	0xF00F,	ToString_NAME_Vx_Vy								},
-	{ "SHL",	SHL_Handler,	0x800E,	0xF00F,	ToString_NAME_Vx								},
-	{ "SNE",	SNE_2_Handler,	0x9000,	0xF00F,	ToString_NAME_Vx_Vy								},
-	{ "LD",		LD_I_Handler,	0xA000,	0xF000,	std::bind(ToString_NAME_dst_nnn, _1, _2, "I")	},
-	{ "JP",		JP_2_Handler,	0xB000,	0xF000,	std::bind(ToString_NAME_dst_nnn, _1, _2, "V0")	},
-	{ "RND",	RND_Handler,	0xC000,	0xF000,	ToString_NAME_Vx_kk								},
-	{ "DRW",	DRW_Handler,	0xD000,	0xF000,	ToString_NAME_Vx_Vy_n							},
-	{ "SKP",	SKP_Handler,	0xE09E,	0xF0FF,	ToString_NAME_Vx								},
-	{ "SKNP",	SKNP_Handler,	0xE0A1,	0xF0FF,	ToString_NAME_Vx								},
-	{ "LD",		LD_3_Handler,	0xF007,	0xF0FF,	std::bind(ToString_NAME_Vx_src, _1, _2, "DT")	},
-	{ "LD",		LD_4_Handler,	0xF00A,	0xF0FF,	std::bind(ToString_NAME_Vx_src, _1, _2, "K")	},
-	{ "LD",		LD_5_Handler,	0xF015,	0xF0FF,	std::bind(ToString_NAME_dst_Vx, _1, _2, "DT")	},
-	{ "LD",		LD_6_Handler,	0xF018,	0xF0FF,	std::bind(ToString_NAME_dst_Vx, _1, _2, "ST")	},
-	{ "ADD",	ADD_I_Handler,	0xF01E,	0xF0FF,	std::bind(ToString_NAME_dst_Vx, _1, _2, "I")	},
-	{ "LD",		LD_7_Handler,	0xF029,	0xF0FF,	std::bind(ToString_NAME_dst_Vx, _1, _2, "F")	},
-	{ "LD",		LD_8_Handler,	0xF033,	0xF0FF,	std::bind(ToString_NAME_dst_Vx, _1, _2, "B")	},
-	{ "LD",		LD_9_Handler,	0xF055,	0xF0FF,	std::bind(ToString_NAME_dst_Vx, _1, _2, "[I]")	},
-	{ "LD",		LD_10_Handler,	0xF065,	0xF0FF,	std::bind(ToString_NAME_Vx_src, _1, _2, "[I]")	},
+	{ "CLS",	Handler_CLS,			0x00E0,	0xF0FF,	ToString_NAME									},
+	{ "RET",	Handler_RET,			0x00EE,	0xF0FF,	ToString_NAME									},
+	{ "JP",		Handler_JP_nnn,			0x1000,	0xF000,	ToString_NAME_nnn								},
+	{ "CALL",	Handler_CALL_nnn,		0x2000,	0xF000,	ToString_NAME_nnn								},
+	{ "SE",		Handler_SE_Vx_kk,		0x3000,	0xF000,	ToString_NAME_Vx_kk								},
+	{ "SNE",	Handler_SNE_Vx_kk,		0x4000,	0xF000,	ToString_NAME_Vx_kk								},
+	{ "SE",		Handler_SE_Vx_Vy,		0x5000,	0xF00F,	ToString_NAME_Vx_Vy								},
+	{ "LD",		Handler_LD_Vx_kk,		0x6000,	0xF000,	ToString_NAME_Vx_kk								},
+	{ "ADD",	Handler_ADD_Vx_kk,		0x7000,	0xF000,	ToString_NAME_Vx_kk								},
+	{ "LD",		Handler_LD_Vx_Vy,		0x8000,	0xF00F,	ToString_NAME_Vx_Vy								},
+	{ "OR",		Handler_OR_Vx_Vy,		0x8001,	0xF00F,	ToString_NAME_Vx_Vy								},
+	{ "AND",	Handler_AND_Vx_Vy,		0x8002,	0xF00F,	ToString_NAME_Vx_Vy								},
+	{ "XOR",	Handler_XOR_Vx_Vy,		0x8003,	0xF00F,	ToString_NAME_Vx_Vy								},
+	{ "ADD",	Handler_ADD_Vx_Vy,		0x8004,	0xF00F,	ToString_NAME_Vx_Vy								},
+	{ "SUB",	Handler_SUB_Vx_Vy,		0x8005,	0xF00F,	ToString_NAME_Vx_Vy								},
+	{ "SHR",	Handler_SHR_Vx,			0x8006,	0xF00F,	ToString_NAME_Vx								},
+	{ "SUBN",	Handler_SUBN_Vx_Vy,		0x8007,	0xF00F,	ToString_NAME_Vx_Vy								},
+	{ "SHL",	Handler_SHL_Vx,			0x800E,	0xF00F,	ToString_NAME_Vx								},
+	{ "SNE",	Handler_SNE_Vx_Vy,		0x9000,	0xF00F,	ToString_NAME_Vx_Vy								},
+	{ "LD",		Handler_LD_I_nnn,		0xA000,	0xF000,	std::bind(ToString_NAME_dst_nnn, _1, _2, "I")	},
+	{ "JP",		Handler_JP_V0_nnn,		0xB000,	0xF000,	std::bind(ToString_NAME_dst_nnn, _1, _2, "V0")	},
+	{ "RND",	Handler_RND_Vx_kk,		0xC000,	0xF000,	ToString_NAME_Vx_kk								},
+	{ "DRW",	Handler_DRW_Vx_Vy_n,	0xD000,	0xF000,	ToString_NAME_Vx_Vy_n							},
+	{ "SKP",	Handler_SKP_Vx,			0xE09E,	0xF0FF,	ToString_NAME_Vx								},
+	{ "SKNP",	Handler_SKNP_Vx,		0xE0A1,	0xF0FF,	ToString_NAME_Vx								},
+	{ "LD",		Handler_LD_Vx_DT,		0xF007,	0xF0FF,	std::bind(ToString_NAME_Vx_src, _1, _2, "DT")	},
+	{ "LD",		Handler_LD_Vx_K,		0xF00A,	0xF0FF,	std::bind(ToString_NAME_Vx_src, _1, _2, "K")	},
+	{ "LD",		Handler_LD_DT_Vx,		0xF015,	0xF0FF,	std::bind(ToString_NAME_dst_Vx, _1, _2, "DT")	},
+	{ "LD",		Handler_LD_ST_Vx,		0xF018,	0xF0FF,	std::bind(ToString_NAME_dst_Vx, _1, _2, "ST")	},
+	{ "ADD",	Handler_ADD_I_Vx,		0xF01E,	0xF0FF,	std::bind(ToString_NAME_dst_Vx, _1, _2, "I")	},
+	{ "LD",		Handler_LD_F_Vx,		0xF029,	0xF0FF,	std::bind(ToString_NAME_dst_Vx, _1, _2, "F")	},
+	{ "LD",		Handler_LD_B_Vx,		0xF033,	0xF0FF,	std::bind(ToString_NAME_dst_Vx, _1, _2, "B")	},
+	{ "LD",		Handler_LD_derefI_Vx,	0xF055,	0xF0FF,	std::bind(ToString_NAME_dst_Vx, _1, _2, "[I]")	},
+	{ "LD",		Handler_LD_Vx_derefI,	0xF065,	0xF0FF,	std::bind(ToString_NAME_Vx_src, _1, _2, "[I]")	},
 };
